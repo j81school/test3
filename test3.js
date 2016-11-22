617d189dbbc4f012c02f0907d8b0a8c52f03468f
j81school <n0x10u5ph0n1k5@gmail.com>
2016-11-21 17:08:39 -0800

    Add files via upload

--- test3.js
+++ test3.js
@@ -1,16 +1,19 @@
-/**
- * Created by ph0 on 11/21/16.
- */
-var imgObj = null;
-var w = parseInt(window.innerWidth);
-var intv;
+/*Joseph Sanchez // CS81 // TEST3 // Javascript */
+var imgObj = null; // imgObj will be loaded on start
+var w = parseInt(window.innerWidth); //Gets the width of the window
+var interval1; // used for setInterval function
+
+/*called by window.onload and loads imgObjc with the robot image
+as well as positions it */
 function init(){
-    imgObj = document.getElementById('myImage');
+    imgObj = document.getElementById('robot');
     imgObj.style.position= 'relative';
     imgObj.style.left = '0px';
 
 }
-
+/*moveRight checks to see if the image is too far to the right
+(as defined by var w) and if it is not, it moves it to the right, if not
+it resets the image to .style.left="0px" at the left of the page */
 function moveRight(){
     if ((parseInt(imgObj.style.left)) < w){
         imgObj.style.left = parseInt(imgObj.style.left) + 10 + 'px';
@@ -19,11 +22,13 @@ function moveRight(){
         imgObj.style.left ='0px';
     }
 }
-
+//stopSliding both clears the interval used and calls changeButton()
 function stopSliding(){
-    clearInterval(intv);
+    clearInterval(interval1);
     changeButton();
 }
+/*This function changes both the value and .onclick action based
+on the initial value*/
 function changeButton(){
     if (document.getElementById("button1").value=="Start"){
         document.getElementById("button1").value="Stop";
@@ -33,10 +38,11 @@ function changeButton(){
         document.getElementById("button1").onclick=slideRight;
     }
 }
+
+//slideRight works automatically at the interval of 50 miliseconds
 function slideRight() {
-    intv = setInterval(moveRight,50);
+    interval1 = setInterval(moveRight,50);
     changeButton();
 }
-
-
+//calls init
 window.onload =init;
